3d52e7d26c29589bc7cccd49f8a391a3fde8aba5
sha jalal <106727351+ShajalalCSE@users.noreply.github.com>
2024-01-31 12:32:13 +0600

    Route setup

--- SignUp/src/App.jsx
+++ SignUp/src/App.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import SignUp from './Components/SignUp';
-import SignIn from './Components/SignIn';
-
  function App() {
   return (
-    <>
-    <SignUp/>
-    <SignIn/>
-    </>
+   
+   <>
+   </>
+   
   );
 }
 export default App;
--- SignUp/src/main.jsx
+++ SignUp/src/main.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// main.jsx
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+import './index.css';
+import SignUpComponent from './Components/SignUpComponent.jsx'
+
+import { Route , RouterProvider , createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import SignIn from './Components/SignIn';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+    <Route path='/' element = {<SignUpComponent/>}/>
+    <Route path='SignIn'element = {<SignIn/>}/>
+    </>
+  )
+)
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
